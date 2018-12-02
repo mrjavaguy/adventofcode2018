4f10e1cdba6d7dbf87ddad78395df2d9c892dbf6
Eric Holton <eric.holton@us.imshealth.com>
2018-12-02 08:41:45 -0800

    Day 1 Clean Up

---
+++ day01.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"adventofcode2018/input"
+	"fmt"
+	"strconv"
+)
+
+func convertStringArrayToIntArray(stringArray []string) []int {
+	var intArray []int
+	for _, str := range stringArray {
+		x, _ := strconv.Atoi(str)
+		intArray = append(intArray, x)
+	}
+	return intArray
+}
+
+func sum(input []int) int {
+	sum := 0
+
+	for i := range input {
+		sum += input[i]
+	}
+
+	return sum
+}
+
+func day01Part1(input []int) int {
+	return sum(input)
+}
+
+func day01Part2(input []int) int {
+	found := false
+	visited := make(map[int]bool)
+	frequency := 0
+	for !found {
+		for i := range input {
+			frequency += input[i]
+			if _, ok := visited[frequency]; ok {
+				found = true
+				break
+			}
+			visited[frequency] = true
+		}
+	}
+
+	return frequency
+}
+
+func main() {
+	lines, _ := input.FileToLines("input01.txt")
+	day01input := convertStringArrayToIntArray(lines)
+	part1 := day01Part1(day01input)
+	part2 := day01Part2(day01input)
+	fmt.Printf("Day 1 Part 1 %v, Part 2 %v", part1, part2)
+}
