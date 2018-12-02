0db13591ae1f11849f051af397f181a4d744e352
Eric Holton <eric.holton@us.imshealth.com>
2018-12-02 08:41:45 -0800

    Day 1 Clean Up

---
+++ input/input.go
@@ -0,0 +1,29 @@
+package input
+
+import (
+	"bufio"
+	"io"
+	"os"
+)
+
+func linesFromReader(r io.Reader) ([]string, error) {
+	var lines []string
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+func FileToLines(filePath string) ([]string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return linesFromReader(f)
+}
